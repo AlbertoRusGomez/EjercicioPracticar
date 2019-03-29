0691fee8b732644ae84eed871134bd57c02a4ed1
alber <alber@DESKTOP-S96FFKC>
2019-03-29 10:10:58 +0100

    Creacion ClaseVista

--- EjercicioGitHub/src/Clases/ClaseVista.java
+++ EjercicioGitHub/src/Clases/ClaseVista.java
@@ -1,5 +1,42 @@
 package Clases;
 
-public class ClaseVista {
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ClaseVista extends JFrame{
+	private static final long serialVersionUID = 1L;
+	
+	JTextField palabra = new JTextField(15);
+	JTextField numero = new JTextField(5);
+	
+	JButton calcular = new JButton("Calcular");
+	
+	JPanel jpanel = new JPanel();
+	
+	public ClaseVista() {
+		setTitle("Contar letras");
+		setSize(320, 250);
+		setLayout(new FlowLayout());
+		
+		jpanel.setPreferredSize(this.getSize());
+		jpanel.setLayout(null);
+		
+		palabra.setBounds(60, 30, 200, 30);
+		jpanel.add(palabra);
+		
+		numero.setBounds(60, 90, 50, 30);
+		numero.setEditable(false);
+		jpanel.add(numero);
+		
+		calcular.setBounds(170, 90, 90, 30);
+		jpanel.add(calcular);
+		
+		add(jpanel);
+		this.setVisible(true);
+	}
 
 }
